c3a2379abf10df48b3fe030f9e980a3036d29de9
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-02-07 15:28:29 +0100

    added find_package(OpenGL) to fix a build issue

--- src/games/morrowind/game_morrowind_en.ts
+++ src/games/morrowind/game_morrowind_en.ts
@@ -61,7 +61,7 @@ Splash by %1</source>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="morrowindgameplugins.cpp" line="122"/>
+        <location filename="morrowindgameplugins.cpp" line="121"/>
         <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
         <translation type="unfinished"></translation>
     </message>
